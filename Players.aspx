1e78605f540038b434c2448ae449073a4dbe651d
ssinghai997 <68274724+ssinghai997@users.noreply.github.com>
2020-08-14 01:27:46 +0530

    Add VS Solution Files

---
+++ Players.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Players.aspx.cs" Inherits="WebDataAccessConnectedAssign.Players" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+            </asp:GridView>
+            <br />
+            Player Name&nbsp;
+            <asp:TextBox ID="txtName" runat="server" OnTextChanged="txtName_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            Sport&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtSport" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btnInsertPlayer" runat="server" OnClick="btnInsertPlayer_Click" Text="Insert Player" />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnInsertSp" runat="server" OnClick="btnInsertSp_Click" Text="Insert Player Using Sp" />
+            <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnInsertPara" runat="server" OnClick="btnInsertPara_Click" Text="Insert Player With Parameter" />
+            <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update Player" />
+            <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnUpdateUsingSp" runat="server" OnClick="btnUpdateUsingSp_Click" Text="Update Player Using Sp" />
+            <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnUpdatePara" runat="server" OnClick="btnUpdatePara_Click" Text="Update Player with parameter" />
+            <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete Player" />
+            <asp:Label ID="Label7" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnDeleteWithSp" runat="server" OnClick="btnDeleteWithSp_Click" Text="Delete Player Using Sp" />
+            <asp:Label ID="Label8" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="btnDeletePara" runat="server" OnClick="btnDeletePara_Click" Text="Delete Player Using Parameter" />
+            <asp:Label ID="Label9" runat="server" Text="Label"></asp:Label>
+&nbsp;&nbsp;
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Players.aspx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebDataAccessConnectedAssign
+{
+    public partial class Players : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-OO42D65;Initial Catalog=AssignmentData;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader sdr;
+
+        public void ShowData()
+        {
+            conn.Open();
+            cmd = new SqlCommand("select * from Players", conn); 
+            sdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            sdr.Close();
+            conn.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShowData();
+            }
+        }
+
+        protected void btnInsertPlayer_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into Players(PlayerName, Sport) values('" + txtName.Text + "', '" + txtSport.Text + "')", conn);
+            int y = cmd.ExecuteNonQuery();
+            Label1.Text = "Player Name Inserted";
+            conn.Close();
+            ShowData();
+        }
+
+        protected void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsertSp_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("sp_InsertPlayers",conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@PlayerName", SqlDbType.VarChar,20).Value = txtName.Text;
+            cmd.Parameters.Add("@sport", SqlDbType.VarChar, 20).Value = txtSport.Text;
+            cmd.ExecuteNonQuery();
+            Label2.Text = "Player Name Inserted with Stored procedure";
+            conn.Close();
+            ShowData();
+        }
+
+        protected void btnInsertPara_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            
+            cmd = new SqlCommand("Insert into Players(PlayerName, Sport) values(@playername, @sport)", conn);
+            cmd.Parameters.Add("@playername", SqlDbType.VarChar,20).Value = txtName.Text;
+            cmd.Parameters.Add("@sport", SqlDbType.VarChar, 20).Value = txtSport.Text;
+            
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                Label3.Text = "Player Name Inserted with parameters";
+            }
+
+            conn.Close();
+            ShowData();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Update Players set PlayerName='"+txtName.Text+"' where Sport = '"+txtSport.Text+"' ", conn);
+            int y = cmd.ExecuteNonQuery();
+            Label4.Text = "Player Name Updated";
+            conn.Close();
+            ShowData();
+        }
+
+        protected void btnUpdateUsingSp_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("sp_UpdatePlayer", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@PlayerName", SqlDbType.VarChar, 20).Value = txtName.Text;
+            cmd.Parameters.Add("@sport", SqlDbType.VarChar, 20).Value = txtSport.Text;
+            cmd.ExecuteNonQuery();
+            Label5.Text = "Player Name Updated using stored procedure" ;
+            conn.Close();
+            ShowData();
+
+        }
+
+        protected void btnUpdatePara_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            cmd = new SqlCommand("update Players set playername=@playername where sport=@sport", conn);
+            cmd.Parameters.Add("@playername", SqlDbType.VarChar, 20).Value = txtName.Text;
+            cmd.Parameters.Add("@sport", SqlDbType.VarChar, 20).Value = txtSport.Text;
+
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                Label6.Text = "Player Name Updated using parameters";
+            }
+
+            conn.Close();
+            ShowData();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("delete from Players where Playername='" + txtName.Text + "' ", conn);
+            cmd.ExecuteNonQuery();
+            Label7.Text = "Player Name Deleted";
+            conn.Close();
+            ShowData();
+        }
+
+        protected void btnDeleteWithSp_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("sp_Deleteplayer", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@playername", SqlDbType.VarChar,20).Value = txtName.Text;
+            cmd.ExecuteNonQuery();
+            Label8.Text = "Player Name Deleted using Stored procedures";
+            conn.Close();
+            ShowData();
+        }
+
+        protected void btnDeletePara_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            cmd = new SqlCommand("delete from players where playername=@playername", conn);
+            cmd.Parameters.Add("@playername", SqlDbType.VarChar, 20).Value = txtName.Text;
+            cmd.Parameters.Add("@sport", SqlDbType.VarChar, 20).Value = txtSport.Text;
+
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                Label9.Text = "Player Name deleted using parameters";
+            }
+
+            conn.Close();
+            ShowData();
+        }
+    }
+    
+}
